67004a5fcda9459165fe9e1ea1ee629ac126cd85
K1ngHandy <a.king.life@pm.me>
2023-10-12 18:55:02 -0400

    this method inside object

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+// Building objects with functionality
+// const dungeonLockCharacter = {
+//     newRoom: function (roomType) {
+//         return `You have now entered the ${roomType} room.`;
+//     }
+// }
+
+// const currentRoom = dungeonLockCharacter.newRoom('dungeon'); // dungeon entered as parameter
+// console.log(currentRoom);
+
+// Referencing the object using this inside methods
 const dungeonLockCharacter = {
-    newRoom: function (roomType) {
-        return `You have now entered the ${roomType} room.`;
+    location: 'dungeon',
+    changeRoom: function (newRoom) { // method to change location
+        this.location = newRoom;
     }
 }
 
-const currentRoom = dungeonLockCharacter.newRoom('dungeon'); // dungeon entered as parameter
-console.log(currentRoom);
+dungeonLockCharacter.changeRoom('arsenal');
+console.log(dungeonLockCharacter.location); // prints 'arsenal'
